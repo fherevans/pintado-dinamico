f86821441ec583d43f6f31586aff61d4619ea86a
luis <mentesniker@gmail.com>
2021-06-03 08:53:51 -0500

    se agrego una mejor validacikon para los checkbox

--- src/App.vue
+++ src/App.vue
@@ -10,17 +10,21 @@
           <div v-else>Hola amiwos</div>
         </li>
       </ul>
+        <div v-for="(widget, index) in widgets" :key="index">
+          <Checkbox @checked="validacionCheckbox" :id="index" :disabled="widget.disabled" :required="widget.required" :max="widget.max" :values="widget.values" :valuesSelected="widget.valuesSelected" :labels="widget.labels"/>
+        </div>
     </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import Checkbox from '@/components/Checkbox';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    'Checkbox' : Checkbox,
   },
   mounted () {
       return console.log(this.preguntas); 
@@ -28,14 +32,56 @@ export default {
   data() {
     return{
       preguntas : [{id : 1, label : 'Tu nombre', tipo : 'texto', valores : {max : 10, min:2, mandatory : true}, respuesta : ''},
-                   {id : 2, label: 'Color favorito', tipo : 'selection', valores : ['verde','rojo','rosa'], respuesta : ''}]
+                   {id : 2, label: 'Color favorito', tipo : 'selection', valores : ['verde','rojo','rosa'], respuesta : ''}],
+      widgets: [
+            {
+              values: ["primeraCosa","foo2","foo3","foo4"],
+              labels : ["primerFoo","foo2","foo3","foo4"],
+              valuesSelected: [true,false,false,false],
+              id:1,
+              min:0,
+              required:true,
+              max:3,
+              disabled: false
+            },
+            {
+              values: ["primeraCosa","foo2","foo3"],
+              labels : ["primerFoo","foo2","foo3"],
+              valuesSelected: [true,false,false],
+              id:1,
+              min:0,
+              required:true,
+              max:3,
+              disabled:false
+            },
+          ],
     }
   },
 
   methods: {  
     addQuestion(){
       this.preguntas.push({id:this.preguntas.length,tipo:'texto',}) 
-    }
+    },
+    validacionCheckbox: function(value, checked, id) {
+      this.widgets[id].valuesSelected[value] = checked;
+      let atLeastOne = false;
+      let numberSelections = 0;
+      this.widgets[id].valuesSelected.forEach((value) => {
+        atLeastOne = atLeastOne || value;
+        if(value){
+            numberSelections += 1;
+        }
+      });
+      if(!atLeastOne && this.widgets[id].required){
+          alert("please select one");
+      }
+      if(numberSelections >= this.widgets[id].max){
+          alert("you can not select another one");
+          this.widgets[id].disabled = true;
+      } else{
+          this.widgets[id].disabled = false;
+      }
+    },
   }
 }
 </script>
